5d8bbdc904d5e49dedf21ec327ee4867ba547032
tituscharlie <tituslifecharlie@gmail.com>
2023-08-31 15:53:06 +0100

    7-binary_tree_inorder.c done

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary tree using in-order traversal
+ * binary_tree_inorder - function that goes through a
+ * binary tree using in-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node
  */
