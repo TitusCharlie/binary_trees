d120c76d4f04a9edd17925a6f1a5f61f5df83fdd
tituscharlie <tituslifecharlie@gmail.com>
2023-08-31 15:12:15 +0100

    6-binary_tree_preorder.c done

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_preorder - function that goes through a
- * 			  binary tree using pre-order traversal
+ * binary tree using pre-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node
  *
